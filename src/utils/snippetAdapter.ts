43ec631de5908c7217d0847d2b058d9bea20077b
Tomás <troldanborjas@mail.austral.edu.ar>
2024-11-11 20:43:19 -0300

    Fixed snippet update

--- src/utils/snippetAdapter.ts
+++ src/utils/snippetAdapter.ts
@@ -5,11 +5,12 @@ export class SnippetAdapter {
     createSnippet(createSnippet: CreateSnippet): FormData {
         const formData = new FormData();
         const fileBlob = new Blob([createSnippet.content], { type: 'text/plain' });
+        const splitLanguage: string[] = createSnippet.language.split("/")
 
         formData.append('name', createSnippet.name);
         formData.append('description', "");
-        formData.append('language', createSnippet.language);
-        formData.append('version', "1.1");
+        formData.append('language', splitLanguage[0]);
+        formData.append('version', splitLanguage[1]);
         formData.append('file', fileBlob, createSnippet.name + "." + createSnippet.extension);
         return formData;
 
